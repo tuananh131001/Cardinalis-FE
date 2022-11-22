85a6163130b402c7ab29b24d985b2f5eef0ba958
Huy ne <bmhuyquoc104@gmail.com>
2022-11-22 16:39:20 +0700

    3. Implement theme switching

---
+++ client/src/styles/Theme.js
@@ -0,0 +1,53 @@
+const lightTheme = {
+  button: {
+    backgroundColor: 'var(--button_text_light)'
+  },
+  body: {
+    backgroundColor: 'var(--background-color_light)'
+  },
+  text: {
+    color: 'var(--text_color_light)'
+  },
+  input: {
+    backgroundColor: 'var(--input_background_light)'
+  },
+  subText: {
+    color: 'var(--subtext_color_light)'
+  },
+  card: {
+    backgroundColor: 'var(--card_background_light)'
+  },
+  placeHolder: {
+    color: 'var(--text_placeholder_light)'
+  },
+  primaryColor: 'var(--primary_color_light)',
+  secondaryColor: 'var(--secondary_color_light)'
+};
+
+const darkTheme = {
+  button: {
+    backgroundColor: 'var(--button_text_dark)'
+  },
+  body: {
+    backgroundColor: 'var(--background-color_dark)'
+  },
+  text: {
+    color: 'var(--text_color_dark)'
+  },
+  input: {
+    backgroundColor: 'var(--input_background_dark)'
+  },
+  subText: {
+    color: 'var(--subtext_color_dark)'
+  },
+  card: {
+    backgroundColor: 'var(--card_background_dark)'
+  },
+  placeHolder: {
+    color: 'var(--text_placeholder_dark)'
+  },
+  primaryColor: 'var(--primary_color_dark)',
+  secondaryColor: 'var(--secondary_color_dark)'
+};
+
+export { lightTheme, darkTheme };
