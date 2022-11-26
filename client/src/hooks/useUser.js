9afae4ec91e418a562c6646bd1aa625a2f23a0ea
Huy ne <bmhuyquoc104@gmail.com>
2022-11-26 10:43:55 +0700

    6. Add user api to communicate with backedn

---
+++ client/src/hooks/useUser.js
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { registerUser, signUp, updateProfile } from '@/api/User';
+
+const useRegister = () =>
+  useMutation({
+    mutationFn: (user) => registerUser(user),
+    onSuccess: (data) => console.log(data),
+    onError: (error) => console.log(error)
+  });
+
+const useSignUp = () =>
+  useMutation({
+    mutationFn: (user) => signUp(user),
+    onSuccess: (data) => console.log(data),
+    onError: (error) => console.log(error)
+  });
+
+const useUpdateProfile = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (user) => updateProfile(user),
+    onSuccess: (data) => queryClient.setQueryData(['todo', { id: data.id }], data),
+    onError: (error) => console.log(error)
+  });
+};
+
+export { useSignUp, useRegister, useUpdateProfile };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
