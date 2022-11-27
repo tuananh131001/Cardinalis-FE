56be44d74665a8d65d3554814fbc3eec4fbefa8d
Matsuri <nhungmaitran1412@gmail.com>
2022-11-27 20:09:04 +0700

    1. Build Text components

--- client/src/components/Text/Text.jsx
+++ client/src/components/Text/Text.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+// ref: https://stackoverflow.com/questions/57945969/conditional-rendering-of-components-with-same-props-in-reactjs
+import { StyledHeading1, StyledHeading2, StyledHeading3, StyledParagraph } from './Text.styled';
 
-function Text() {
-  return <div>Text</div>;
+function Text({ type, themeToggler = null, text, ...props }) {
+  let generalPropsList = {
+    themeToggler: themeToggler,
+    ...props
+  };
+
+  switch (type) {
+    case 'h1':
+      return <StyledHeading1 {...generalPropsList}>{text}</StyledHeading1>;
+    case 'h2':
+      return <StyledHeading2 {...generalPropsList}>{text}</StyledHeading2>;
+    case 'h3':
+      return <StyledHeading3 {...generalPropsList}>{text}</StyledHeading3>;
+    case 'p':
+      return <StyledParagraph {...generalPropsList}>{text}</StyledParagraph>;
+  }
 }
 
 export default Text;
