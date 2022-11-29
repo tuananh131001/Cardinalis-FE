efe74a0eda3037730fc53e69d32c7496e6fc74b4
Huy ne <bmhuyquoc104@gmail.com>
2022-11-29 22:47:14 +0700

    9. Finish dockerize client of the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,25 @@ import GlobalStyled from '@/styles/GlobalStyled';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
-
 import useTheme from '@/hooks/useTheme';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from '@/styles/Theme';
+// import Text from './components/Text/Text';
 
 function App() {
   const [theme, themeToggler] = useTheme();
   const themeMode = theme === 'lightTheme' ? lightTheme : darkTheme;
 
   return (
-    <ThemeProvider theme={themeMode}>
-      <GlobalStyled />
-      <Routes>
-        <Route path="/" element={<Home themeToggler={themeToggler} />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </ThemeProvider>
+    <div className="app">
+      <ThemeProvider theme={themeMode}>
+        <GlobalStyled />
+        <Routes>
+          <Route path="/" element={<Home themeToggler={themeToggler} />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
+    </div>
   );
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,16 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173 // you can replace this port with any port
+  },
   resolve: {
+    // eslint-disable-next-line no-undef
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   },
   plugins: [react()]
