18f43640af716d5491d085f6f7f0f762016c37b6
Huy ne <bmhuyquoc104@gmail.com>
2022-12-21 21:36:52 +0700

    Sever-fix: Change login endpoint

--- client/src/assets/constantEnv.js
+++ client/src/assets/constantEnv.js
@@ -1,5 +1,5 @@
 export const API_ORIGIN = import.meta.env.VITE_REACT_APP_API_ORIGIN || 'http://localhost:9002';
-export const LOGIN_ENDPOINT = import.meta.env.VITE_REACT_APP_LOGIN_ENDPOINT || 'auth/login';
+export const LOGIN_ENDPOINT = import.meta.env.VITE_REACT_APP_LOGIN_ENDPOINT || 'user/login';
 export const REGISTER_ENDPOINT =
   import.meta.env.VITE_REACT_APP_REGISTER_ENDPOINT || 'user/register';
 export const LOGOUT_ENDPOINT = import.meta.env.VITE_REACT_APP_LOGOUT_ENDPOINT || 'user/logout';
