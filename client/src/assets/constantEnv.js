93ab1cce0e73ae61ac262d8af80c10f38427fd93
Huy ne <bmhuyquoc104@gmail.com>
2022-12-29 21:40:37 +0700

    Client-feat: Handle tweet api from backend

--- client/src/assets/constantEnv.js
+++ client/src/assets/constantEnv.js
@@ -4,3 +4,6 @@ export const REGISTER_ENDPOINT =
   import.meta.env.VITE_REACT_APP_REGISTER_ENDPOINT || 'user/register';
 export const LOGOUT_ENDPOINT = import.meta.env.VITE_REACT_APP_LOGOUT_ENDPOINT || 'user/logout';
 export const USER_ENDPOINT = import.meta.env.VITE_REACT_APP_USER_ENDPOINT || 'user';
+export const TWEET_ENDPOINT = import.meta.env.VITE_REACT_APP_TWEET_ENDPOINT || 'tweets/';
+export const TWEET_DETAIL_ENDPOINT =
+  import.meta.env.VITE_REACT_APP_TWEET_DETAIL_ENDPOINT || 'tweet/';
