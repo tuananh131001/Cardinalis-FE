56681e3f7bef7d5e8515f38099f1ccc656ebc3eb
Huy ne <bmhuyquoc104@gmail.com>
2022-12-29 21:53:05 +0700

    Client-feat: Add token to request header

--- client/src/api/Tweet.js
+++ client/src/api/Tweet.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { API_ORIGIN, TWEET_ENDPOINT, TWEET_DETAIL_ENDPOINT } from '@/assets/constantEnv';
 const tweetApi = axios.create({
-  baseURL: API_ORIGIN
+  baseURL: API_ORIGIN,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('userToken')}`
+  }
 });
 
 const postTweet = (tweet) => tweetApi.post(TWEET_ENDPOINT, tweet).then((res) => res.data);
