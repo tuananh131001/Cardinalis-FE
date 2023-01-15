75c967d41d901e17e94d683ba274a551679b12fe
WilliamA1pha <s3864077@rmit.edu.vn>
2023-01-15 16:27:24 +0700

    fix

--- client/src/pages/TokenRecevied.jsx
+++ client/src/pages/TokenRecevied.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 function TokenRecevied() {
   const params = useParams();
-  console.log(params)
+  console.log(params);
   return (
     <div>
       <h1>Token Received</h1>
