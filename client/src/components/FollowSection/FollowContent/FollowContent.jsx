296de65345718b010bc055f3ad8314ed6fb7175b
Huy ne <bmhuyquoc104@gmail.com>
2023-01-16 01:05:57 +0700

    fix bug in navigate follower + following

--- client/src/components/FollowSection/FollowContent/FollowContent.jsx
+++ client/src/components/FollowSection/FollowContent/FollowContent.jsx
@@ -4,36 +4,33 @@ import HeaderSection from '@/components/HeaderSection/HeaderSection';
 import FollowList from './FollowList';
 import PropTypes from 'prop-types';
 import SubNav from '@/components/NavSection/SubNav';
-import { useParams } from 'react-router-dom';
 import { useGetUserFollowers, useGetUserFollowing } from '@/hooks/useUser';
 
 function FollowContent({ type, user }) {
-  const params = useParams();
-  console.log(params);
-  console.log(user);
   const {
     data: followers,
     isLoading: isLoadingFollowers,
     isError: isErrorFollower
-  } = useGetUserFollowers(user.id);
+  } = useGetUserFollowers(user?.data?.id);
   const {
     data: following,
     isLoading: isLoadingFollowing,
     isError: isErrorFollowing
-  } = useGetUserFollowing(user.id);
+  } = useGetUserFollowing(user?.data?.id);
   if (isLoadingFollowers || isLoadingFollowing) return <div>Loading...</div>;
   if (isErrorFollower || isErrorFollowing) return <div>Error</div>;
+  console.log(user?.data.username);
   const followList = type == 'followers' ? followers?.data : following?.data;
 
   return (
     <FollowContentStyled>
       <HeaderSection
         content="Follow"
-        subContent={`@${user.username}`}
-        backDestination={`/${user.username}`}
+        subContent={`@${user?.data.username}`}
+        backDestination={`/${user?.data.username}`}
         leftType="back"
       />
-      <SubNav user={user} navType="followNav" />
+      <SubNav user={user?.data} navType="followNav" />
       <FollowList followList={followList} />
     </FollowContentStyled>
   );
